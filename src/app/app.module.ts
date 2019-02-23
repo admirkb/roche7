6932e05411e55e23fa7216110264959f13b12c95
Kelvin <kelvin@admir.tv>
2019-02-23 14:08:22 +0000

    Remove todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VirtualScrollingModule } from './virtual-scrolling/virtual-scrolling.module'
-import { TodoMachineComponent } from './todo-machine/todo-machine.component';
 
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
@@ -13,12 +12,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { FormsModule } from '@angular/forms';
 
 import { reducers } from './store/reducers';
-import { TodoModule } from './todo.module';
-import { todoReducer } from './store/reducers/todo.reducer';
-// import { BugsMachineComponent } from './bugs-machine/bugs-machine.component';
 
-import { BugModule } from './bug.module';
-import { BugsModule } from './bugs/bugs.module';
 import { BugsDeleteDialogueComponent } from './bugs/bugs-delete-dialogue/bugs-delete-dialogue.component';
 import { BugsAddDialogueComponent } from './bugs/bugs-add-dialogue/bugs-add-dialogue.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,8 +24,6 @@ import { Roche7Module } from './roche7/roche7.module';
 @NgModule({
   declarations: [
     AppComponent,
-    TodoMachineComponent,
-    // BugsMachineComponent
   ],
   imports: [
     BrowserModule,
@@ -45,9 +37,6 @@ import { Roche7Module } from './roche7/roche7.module';
       maxAge: 80 //  Retains last 80 states
     }),
     EffectsModule.forRoot([]),
-    TodoModule,
-    // BugModule,
-    // BugsModule,
     MaterialDesignModule,
     Roche7Module
   ],
